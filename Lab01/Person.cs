ddcb92d7adf23b313d8b67fe261fb5258edea710
jmalecki <maleckijakub@o2.pl>
2019-03-13 18:47:02 +0100

    Wersja 1

---
+++ Lab01/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Lab01
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Image Image1 { get; set; }
+    }
+}
