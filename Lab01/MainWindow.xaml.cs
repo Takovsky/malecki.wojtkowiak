a724389da6b90117e9a5f970a738f51eeb09ed89
Pawel Wojtkowiak <pawelwojtkowiak96@gmail.com>
2019-03-20 17:18:10 +0100

    cleaned additional white spaces and unnecessary namespaces usage

--- Lab01/MainWindow.xaml.cs
+++ Lab01/MainWindow.xaml.cs
@@ -1,19 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Lab01
 {
@@ -23,7 +15,8 @@ namespace Lab01
     public partial class MainWindow : Window
     {
         ObservableCollection<Person> people = new ObservableCollection<Person>
-        { };
+        {
+        };
 
         public ObservableCollection<Person> Items
         {
@@ -36,15 +29,13 @@ namespace Lab01
             DataContext = this;
         }
 
-
-
         private void AddNewPersonButton_Click(object sender, RoutedEventArgs e)
         {
-            
+
             bool isNumeric = int.TryParse(ageTextBox.Text, out int n); // Czy wiek to liczba
 
-            if ( !String.IsNullOrEmpty(ageTextBox.Text) && !String.IsNullOrEmpty( nameTextBox.Text) && (Obraz.Source != null )
-                && (isNumeric==true))
+            if (!String.IsNullOrEmpty(ageTextBox.Text) && !String.IsNullOrEmpty(nameTextBox.Text) && ( Obraz.Source != null )
+                && ( isNumeric == true ))
             {
                 people.Add(new Person { Age = int.Parse(ageTextBox.Text), Name = nameTextBox.Text, Image1 = new Image() { Source = Obraz.Source } });
 
@@ -60,18 +51,13 @@ namespace Lab01
                 ageTextBox.Text = string.Empty;
                 nameTextBox.Text = string.Empty;
                 Obraz.Source = null;
-
-
-
-
             }
-            
         }
 
         private void LoadPicture(object sender, RoutedEventArgs e)
         {
             String filepath = "";
-            OpenFileDialog fileDialog = new OpenFileDialog() { Filter = " (*.jpg)|*.jpg| (*.jpeg)|*.jpeg| (*.gif)|*.gif| (*.png)|*.png| (*.pgm)|*.pgm"  };
+            OpenFileDialog fileDialog = new OpenFileDialog() { Filter = " (*.jpg)|*.jpg| (*.jpeg)|*.jpeg| (*.gif)|*.gif| (*.png)|*.png| (*.pgm)|*.pgm" };
             if (fileDialog.ShowDialog() == true)
             {
                 filepath = fileDialog.FileName; // w stringu mamy sciezke
@@ -80,30 +66,23 @@ namespace Lab01
             }
         }
 
-
         private void ShowData(object sender, MouseButtonEventArgs e)
         {
-
-                var osoba = ListBox1.SelectedItem as Person;
+            var osoba = ListBox1.SelectedItem as Person;
 
             if (osoba != null)
             {
-
-                Image2.Source = osoba.Image1.Source; 
+                Image2.Source = osoba.Image1.Source;
                 Tekst2.Text = "Age:   " + osoba.Age.ToString();
-                Tekst1.Text = "Name:  " +  osoba.Name;
+                Tekst1.Text = "Name:  " + osoba.Name;
             }
-            
         }
-
 
         private void Mysz1(object sender, MouseEventArgs e)
         {
             addNewPersonButton.Foreground = Brushes.Black;
-            
+
         }
-
-
 
         private void Mysz2(object sender, MouseEventArgs e)
         {
@@ -112,21 +91,15 @@ namespace Lab01
             addNewPersonButton.Foreground = Brushes.White;
         }
 
-
-
         private void Mysz33(object sender, MouseEventArgs e)
         {
             PhotoButton.Foreground = Brushes.Black;
         }
 
-
-
         private void Mysz44(object sender, MouseEventArgs e)
         {
             var bc = new BrushConverter();
             PhotoButton.Foreground = Brushes.White;
-            
-
         }
     }
 }
