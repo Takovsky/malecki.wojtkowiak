ccc162dc1add89d60370ce540f99dcafdffa24b0
Pawel Wojtkowiak <pawelwojtkowiak96@gmail.com>
2019-03-20 19:48:33 +0100

    added async counting down timer

--- Lab01/MainWindow.xaml.cs
+++ Lab01/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -100,6 +102,106 @@ namespace Lab01
         {
             var bc = new BrushConverter();
             PhotoButton.Foreground = Brushes.White;
+        }
+
+        protected void timerValueTextBlockOffTimer(TextBox textBlock)
+        {
+            try
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+            {
+                textBlock.IsReadOnly = false;
+                textBlock.Foreground = Brushes.Black;
+                textBlock.Background = Brushes.White;
+            });
+            }
+            catch { }
+        }
+
+        protected void timerValueTextBlockOnTimer(TextBox textBlock)
+        {
+            try
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+            {
+                textBlock.IsReadOnly = true;
+                textBlock.Foreground = Brushes.Blue;
+                textBlock.Background = Brushes.DarkGray;
+            });
+            }
+            catch { }
+        }
+
+        protected void updateTimerValueTextBoxText(string text, TextBox textBlock)
+        {
+            try
+            {
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    textBlock.Text = text;
+                });
+            }
+            catch { }
+        }
+
+        class TimerCountdown
+        {
+            int mMs;
+            int mCountdownBreak;
+            MainWindow mSender;
+
+            public TimerCountdown(MainWindow sender, int ms, int countdownBreak)
+            {
+                mSender = sender;
+                mMs = ms;
+                mCountdownBreak = countdownBreak;
+            }
+
+            public void updateStatus(Object stateInfo)
+            {
+                if (mMs < 0)
+                {
+                    mMs = 0;
+                }
+                mSender.updateTimerValueTextBoxText(mMs.ToString() + "ms", mSender.timerValueTextBox);
+                mMs -= mCountdownBreak;
+            }
+        }
+
+        async Task<int> countdownAsync(int ms, int countdownBreak)
+        {
+            int result = 0;
+            while (result < ms)
+            {
+                result += countdownBreak;
+                await Task.Delay(countdownBreak);
+            }
+            return ms;
+        }
+
+        private async void StartTimerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(timerValueTextBox.Text, out int num))
+            {
+                MessageBox.Show("Not a number");
+                return;
+            }
+            int ms = int.Parse(timerValueTextBox.Text);
+
+            if (ms < 0)
+            {
+                MessageBox.Show("Not a positive number");
+                return;
+            }
+
+            int countdownBreak = 10;
+            timerValueTextBlockOnTimer(timerValueTextBox);
+            var number = countdownAsync(ms, countdownBreak);
+            System.Threading.Timer timer = new System.Threading.Timer(new TimerCountdown(this, ms, countdownBreak).updateStatus,
+                null, TimeSpan.FromMilliseconds(0), TimeSpan.FromMilliseconds(countdownBreak));
+            var result = await number;
+            timer.Dispose();
+            timerValueTextBlockOffTimer(timerValueTextBox);
         }
     }
 }
